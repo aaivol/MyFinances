b238553aad5dcdaacf5a0f7bc2f27333fc36b3f1
Anastasia Ivanova <ivaa1071@gmail.com>
2022-10-10 21:06:27 +0300

    добавлены страницы: основная и регистрации

--- app/src/main/java/com/example/myfinances/MainActivity.java
+++ app/src/main/java/com/example/myfinances/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.myfinances;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void startSecondActivity(View v) {
+        Intent intent = new Intent(this, SecondActivity.class);
+        startActivity(intent);
+    }
+
+    public void startRegActivity(View v) {
+        Intent intent = new Intent(this, RegActivity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/myfinances/RegActivity.java
@@ -0,0 +1,21 @@
+package com.example.myfinances;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class RegActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reg);
+    }
+
+    public void startMainActivity(View v) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
