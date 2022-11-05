ce975a57f45718c7ff047a6489729818e6c7b41e
Anastasia Ivanova <ivaa1071@gmail.com>
2022-11-05 14:55:20 +0300

    настройка AlertDialog

--- app/src/main/java/com/example/myfinances/BuyList.java
+++ app/src/main/java/com/example/myfinances/BuyList.java
@@ -1,10 +1,15 @@
 package com.example.myfinances;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class BuyList extends AppCompatActivity {
 
@@ -12,5 +17,38 @@ public class BuyList extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_buy_list);
+    }
+
+    public void CreateAddDialog(View v){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(R.string.add_dialog);
+        builder.setMessage(R.string.add_msg);
+
+        ConstraintLayout cl = (ConstraintLayout) getLayoutInflater().inflate(R.layout.activity_dialog_add, null);
+        Spinner mySpinner = (Spinner) cl.findViewById(R.id.spinner1);
+
+        ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(BuyList.this,
+                android.R.layout.simple_spinner_item,
+                getResources().getStringArray(R.array.categories));
+        myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mySpinner.setAdapter(myAdapter);
+
+        builder.setView(cl);
+
+        builder.setPositiveButton(R.string.add_button, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //
+            }
+        });
+        builder.setNegativeButton(R.string.close, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 }
